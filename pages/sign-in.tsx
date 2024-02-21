ea589e1dd4c5b0f9901c30c18b7fdfbc8fa27ce8
Stepan Maksimov <neydyxd@yandex.ru>
2024-02-21 10:16:09 +0300

    Добавлен функционал авторизации

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,24 +1,70 @@
 import Link from "next/link";
 import styles from "../styles.module.css";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 interface Props {
   serverInfo: string;
 }
 
 export default function Signin({ serverInfo }: Props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const loginData = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Success:", data);
+        router.push('/profile')
+      } else {
+        const errorData = await response.json();
+        console.error("Error:", errorData);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <>
       <Link href="/" className={styles.about__link}>
         Главная
       </Link>
       <div className={styles.form__container}>
-        <form className={styles.form}>
+        <form onSubmit={handleLogin} className={styles.form}>
           <h2>Авторизация</h2>
-          <input type="text" placeholder="E-mail" />
-          <input type="text" placeholder="Пароль" />
-          <Link className={styles.form__button} href="/profile">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button className={styles.form__button} type="submit">
             Войти
-          </Link>
+          </button>
         </form>
         <p>{serverInfo}</p>
       </div>
