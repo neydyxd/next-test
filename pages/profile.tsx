2c1f30c6faf00103baa39e7882a65ca1fbb6fad8
Stepan Maksimov <neydyxd@yandex.ru>
2024-02-29 10:15:04 +0300

    Фикс страницы profile, теперь она отрисовывается на стороне клиента

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,16 +1,30 @@
-export default function Profile() {
+import { useEffect, useState } from "react";
+
+const Profile = () => {
+  const [isRender, setIsRender] = useState(false);
+
+  useEffect(() => {
+    setIsRender(true);
+  }, []);
+
   return (
     <div>
-      <h2>Профиль пользователя</h2>
-      <p>Возраст: 23</p>
-      <p>Рост: 187</p>
-      <h3>Навыки</h3>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
+      {isRender && (
+        <>
+          <h2>Профиль пользователя</h2>
+          <p>Возраст: 23</p>
+          <p>Рост: 187</p>
+          <h3>Навыки</h3>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+          </ul>
+        </>
+      )}
     </div>
   );
-}
+};
+
+export default Profile;
